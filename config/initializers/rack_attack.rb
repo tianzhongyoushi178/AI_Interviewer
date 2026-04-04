0285ae2eaeed19e7fabfb08a547fcbc457b027ca
田中優史 <tianzhongyoushi178@gmail.com>
2026-04-04 21:36:25 +0900

    fix: rack-attack 6.8.0のcache API変更に対応
    Rack::Attack.cache.store= が廃止されたため
    Rack::Attack.cache_store= に変更

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -6,9 +6,9 @@
 class Rack::Attack
   # キャッシュストア設定
   if Rails.env.production?
-    Rack::Attack.cache.store = ActiveSupport::Cache::RedisCacheStore.new(url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1'))
+    Rack::Attack.cache_store = ActiveSupport::Cache::RedisCacheStore.new(url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1'))
   else
-    Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+    Rack::Attack.cache_store = ActiveSupport::Cache::MemoryStore.new
   end
 
   ### ホワイトリスト ###
